576d48456b18a4976bd8375950d756e88d8dc7a6
Arturo Arenas <arenas_art@hotmail.com>
2019-03-29 13:13:20 -0600

    Adding CRUD functionality to the UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from './components/Home';
 import Configuration from './components/Configuration';
 import Elements from './components/Elements';
