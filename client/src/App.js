a3a6c1f9aa7d02fefd7f95baf3742261fb336f86
Arturo Arenas <arenas_art@hotmail.com>
2019-04-05 20:02:53 -0600

    Adding the UI for the main page (Home)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from './components/Home';
 import Configuration from './components/Configuration';
 import Elements from './components/Elements';
 import Grading from './components/Grading';
 import Overall from './components/Overall';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faTimes, faExclamation } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+library.add(faCheck, faTimes, faExclamation);
 
 class App extends Component {
   render() {
--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
-import { Link as _Link } from 'react-router-dom';
+import { Jumbotron, Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Card, CardImg, CardBody, CardDeck } from 'reactstrap';
 
-const Home = () => (
-    <h1>This is the Home page</h1>
-);
+const CardDiv = (props) => {
+    return (
+        <CardDeck>
+          <Card>
+            <CardImg top width="100%" src="https://media-cdn.sygictraveldata.com/media/800x600/612664395a40232133447d33247d383335393439323831" alt="Card image cap" />
+            <CardBody>
+                <p className="lead">Elementos de Evaluación</p>
+                <hr className="my-2" />
+                <p>En esta sección podrá ver y editar la lista de alumnos, información de los alumnos, materias y términos de evaluación.</p>
+              <Button>Entrar</Button>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardImg top width="100%" src="https://media-cdn.sygictraveldata.com/media/800x600/612664395a40232133447d33247d383335393439323831" alt="Card image cap" />
+            <CardBody>
+                <p className="lead">Evaluación de Término por Materia</p>
+                <hr className="my-2" />
+                <p>En esta sección podrá ver cada rubro que compone la calificación de un término de ciclo escolar. También podrá ver y editar las calificaciones de los alumnos en cada uno de los rubros. La evaluación es por término y materia, usted podrá navegar entre los distintos términos y materias disponibles en el curso.</p>
+              <Button>Entrar</Button>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardImg top width="100%" src="https://media-cdn.sygictraveldata.com/media/800x600/612664395a40232133447d33247d383335393439323831" alt="Card image cap" />
+            <CardBody>
+                <p className="lead">Reportes y Estadísticas Generales</p>
+                <hr className="my-2" />
+                <p >En esta sección podrá ver reportes y estadísticas sobre el desempeño de los alumnos. Encontrará los reportes principales como la tabla de promedios generales y más.</p>
+              <Button>Entrar</Button>
+            </CardBody>
+          </Card>
+        </CardDeck>
+      );
+};
+
+const Home = (props) => {
+  return (
+    <div>
+      <Jumbotron>
+        <h1 className="display-3" style={{ textAlign: "center" }}>Sistema de Evaluación para Primaria</h1>
+        <hr className="my-2" />
+        <CardDiv/>
+        <hr className="my-2" />
+        <p>You can find the whole code of this web application on its Github repository by clicking the link below</p>
+        <p className="lead">
+            <Button color="primary" href="https://github.com/ArthArenas/ClassManager">See Github Repo</Button>
+        </p>
+      </Jumbotron>
+      
+    </div>
+  );
+};
 
 export default Home;
